3ac3e66607a162c83094a97a3f05fcc4b4ae1c5d
Yashwant Gaikwad <you@example.com>
2021-02-08 12:38:20 +0530

    ''

--- Q1_YashwantGaikwad.py
+++ Q1_YashwantGaikwad.py
@@ -1,25 +1,11 @@
+from collections import Counter
 string = "BOOKKEEPER"
-two = []
-three = []
-new = ""
 
-  
-for i in range(0, len(string)):  
-    count = 1;  
-    for j in range(i+1, len(string)):  
-        if(string[i] == string[j] and string[i] != ' '):  
-            count = count + 1;    
+oup = Counter(string)
 
-    if(count > 1 and string[i] != '0'):  
-        if(count == 2):
-            two.append(string[i])
-        elif (count > 2):
-            three.append(string[i])
-
-for x in two:
-    new = string.replace(x, "Z")
-
-for y in three:
-    new = new.replace(y,"X")
-
-print(new)
+for k in oup.keys():
+    if(oup[k] == 2):
+        string = string.replace(k, "Z")
+    if(oup[k] > 2):
+        string = string.replace(k, "X")
+print(string)
--- Q4_YashwantGaikwad.py
+++ Q4_YashwantGaikwad.py
@@ -1,44 +1,15 @@
-# def divisorSum( n ):
-#     sum = 0
-#     for i in range(1, n):
-#         # Find all divisors of i
-#         # and add them
-#         j = 1
-#         while j * j <= i:
-#             if i % j == 0:
-#                 if i / j == j:
-#                     sum += j
-#                 else:
-#                     sum += j + i / j
-#             j = j + 1
-#     return int(sum)
-
-# def divisorSum( n ):
-#     sum = 0
-#     for i in range(1, n):
-#         # Find all divisors of i
-#         # and add them
-#        if(n % i == 0):
-#         #    noOfDivisors = noOfDivisors + 1
-#            sum = sum + i
-#     return int(sum)
- 
-# # Driver code
-# num = int(input("Enter number :"))
-# print( divisorSum(num))
-
 def printDivisors(n) : 
     i = 1
     sumOfDivisors = 0
     noOfDivisors = 0
     while i <= n : 
-        if (n % i==0) : 
-            print(i)
+        if (n % i == 0) : 
             noOfDivisors = noOfDivisors + 1
             sumOfDivisors = sumOfDivisors + i 
         i = i + 1
-    return {'sum_': sumOfDivisors, 'divisors': noOfDivisors}
- 
-answer = printDivisors(25)
-print('sum of divisors is',answer[sum_])
-print('no of divisors are',answer[divisors])
+    return {'sum': sumOfDivisors, 'divisors': noOfDivisors}
+
+num = int(input("Enter number :"))
+answer = printDivisors(num)
+print('sum of divisors is',answer['sum'])
+print('no of divisors are',answer['divisors'])
