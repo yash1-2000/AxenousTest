a0bec1259082c37b1d1e3c2e14af8946a47ec4b9
Yashwant Gaikwad <you@example.com>
2021-02-08 10:40:23 +0530

    Q1

---
+++ Q1_YashwantGaikwad.py
@@ -0,0 +1,27 @@
+string = "BOOKKEEPER"
+two = []
+three = []
+new = ""
+
+  
+for i in range(0, len(string)):  
+    count = 1;  
+    for j in range(i+1, len(string)):  
+        if(string[i] == string[j] and string[i] != ' '):  
+            count = count + 1;    
+
+    if(count > 1 and string[i] != '0'):  
+        if(count == 2):
+            two.append(string[i])
+        elif (count > 2):
+            three.append(string[i])
+
+for x in two:
+    new = string.replace(x, "Z")
+
+for y in three:
+    new = new.replace(y,"X")
+
+print(new)
+
+
